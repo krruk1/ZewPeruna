0f8580d0c6a3e7eed381ef925e0740c764e346ce
informatykastosowana <jakub.tomal@smcebi.edu.pl>
2019-05-06 21:39:10 +0200

    dadana możliwość budowania wież oraz obsługa poruszania się kamery

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/CameraControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+    public float panSpeed = 30f;
+    public float panBorderTickness = 10f;
+    public float scrollSpeed = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderTickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime , Space.World);
+        }
+        if(Input.GetKey("s") || Input.mousePosition.y <= 0 + panBorderTickness )
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderTickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= 0 + panBorderTickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * scrollSpeed * Time.deltaTime;
+
+        transform.position = pos;
+
+    }
+}
