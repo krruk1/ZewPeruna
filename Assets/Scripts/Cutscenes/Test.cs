52045d9e524cd2a2823fa095683179498f695a34
patrykkrokosz <patryk.krokosz@smcebi.edu.pl>
2019-05-07 22:37:27 +0200

    Zalążek cutscenek i kolejna reorganizacja plików.

---
+++ Assets/Scripts/Cutscenes/Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    TextBoxSystem dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = TextBoxSystem.instance;
+    }
+
+    public string[] s = new string[]{
+        "Zawżdy była to ",
+        "nie najlepsza noc.\n",
+        "Doprawydż nie wiem, ",
+        "jak to elegancko przetestować. ",
+        "A jest już późno... A to dopiero początek \"zabawy\" heh.\n",
+        "Teraz tylko pozostaje nie zasnąć...\n",
+        "Nie myśleć o spannn...\n",
+        "Z\n Z\n  Z\n   Z\n    Z\n     Z\n      Z\n       Z\n        Z\n         Z" +
+        "\n          Z\n           Z\n            Z\n             Z\n              Z\n..."
+    };
+    int indx = 0;
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            if (!dialogue.isSpeaking || dialogue.isWaitingForUserInput) {
+                if (indx >= s.Length) {
+                    dialogue.Say("Nie mam pomysłu jak dobrze ogarnąć to ściernisko, ale kiedyś będzie to San Francisco.");
+                    return;
+                }
+                dialogue.SayAdd(s[indx]);
+                indx++;
+            }
+        }
+    }
+
+/*
+    void Say(string s) {
+//        dialogue.Say(s);
+        dialogue.SayAdd(s);
+    }
+*/
+}
---
+++ Assets/Scripts/Cutscenes/TextBoxSystem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxSystem : MonoBehaviour
+{
+    public static TextBoxSystem instance;
+
+    public ELEMENTS elements;
+
+    void Awake() {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    public void Say(string speech) {
+        StopSpeaking();
+        speaking = StartCoroutine(Speaking(speech, false));
+    }
+
+    public void SayAdd(string speech) {
+        StopSpeaking();
+        speechText.text = targetSpeech;
+        speaking = StartCoroutine(Speaking(speech, true));
+    }
+
+    public void StopSpeaking() {
+        if (isSpeaking) {
+            StopCoroutine(speaking);
+        }
+        speaking = null;
+    }
+
+    public bool isSpeaking { get { return speaking != null; } }
+    [HideInInspector] public bool isWaitingForUserInput = false;
+
+    string targetSpeech = "";
+    Coroutine speaking = null;
+    IEnumerator Speaking(string speech, bool additive) {
+        textPanel.SetActive(true);
+        targetSpeech = speech;
+        if (!additive)
+            speechText.text = "";
+        else
+            targetSpeech = speechText.text + targetSpeech;
+        isWaitingForUserInput = false;
+
+        while(speechText.text != targetSpeech) {
+            speechText.text += targetSpeech[speechText.text.Length];
+            yield return new WaitForEndOfFrame();
+        }
+
+        //text finished
+        isWaitingForUserInput = true;
+
+        while (isWaitingForUserInput)
+            yield return new WaitForEndOfFrame();
+
+        StopSpeaking();
+    }
+
+    [System.Serializable]
+    public class ELEMENTS {
+        public GameObject textPanel;
+        public Text speechText;
+    }
+    public GameObject textPanel { get { return elements.textPanel; } }
+    public Text speechText { get { return elements.speechText;  } }
+}
