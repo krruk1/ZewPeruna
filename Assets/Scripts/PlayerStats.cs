af3c5aedd8a3874067ac1e5c567de911e806f388
informatykastosowana <jakub.tomal@smcebi.edu.pl>
2019-05-29 16:03:50 +0200

    adding braking pliking

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static int money;
+    public int startMoney = 400;
+    public static int lifes;
+    public int startLifes = 3; 
+    [SerializeField] TextMeshProUGUI moneyText;
+    [SerializeField] TextMeshProUGUI lifesText;
+    [SerializeField] SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        money = startMoney;
+        lifes = startLifes;
+    }
+
+    private void Update()
+    {
+        moneyText.text = money.ToString();
+        lifesText.text = lifes.ToString();
+        if(lifes <= 0 )
+        {
+            sceneLoader.LoadGameOverScene();
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public TurretBluepriont stamdardTurret;
+    public TurretBluepriont cannon;
+    BuildManager buildManager;
+
+    private void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectStandardTurret()
+    {
+        Debug.Log("kupiłeś wieze");
+        buildManager.SelectTurretToBuild(stamdardTurret);
+    }
+
+    public void SelectCannon()
+    {
+        Debug.Log("kupiłeś wieze 2");
+        buildManager.SelectTurretToBuild(cannon);
+
+    }
+}
